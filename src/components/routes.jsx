edba1c08191e9c114c275d1c44d32a4473ce847b
otterslacks <antonelloluke@gmail.com>
2023-08-12 17:36:34 -0400

    Fixed routes.jsx error

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import About from './About';
 import Portfolio from './Portfolio';
 import Contact from './Contact';
