438fa5ee06b27cc9fa99e28e33a803b447d892ff
otterslacks <antonelloluke@gmail.com>
2023-08-12 13:06:43 -0400

    New files, updated package.json

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="about">
+      <img src="avatar.jpg" alt="Avatar" />
+      <p>Short bio about the developer...</p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={About} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/resume" component={Resume} />
+    </Switch>
+  );
+};
+
+export default Routes;
