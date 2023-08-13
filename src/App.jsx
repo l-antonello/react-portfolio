471fc1eac749bb35cd947309301a489a1b7d7445
otterslacks <antonelloluke@gmail.com>
2023-08-13 15:41:35 -0400

    Header working

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styled-components/App.css';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
-import Footer from './components//Footer';
+import Footer from './components/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-
 
 function App() {
     return (
-        <>
-            {/* <Header /> */}
-            <About />
-            <Portfolio />
-            <Contact />
-            <Resume />
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/resume" element={<Resume />} />
+            </Routes>
             <Footer />
-        </>
+        </Router>
     );
 }
 
