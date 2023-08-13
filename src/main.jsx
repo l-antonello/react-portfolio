69ccafc1c349ed03bc6c1b0ac2f8c6380294bd1d
otterslacks <antonelloluke@gmail.com>
2023-08-12 20:21:16 -0400

    edits to main.jsx to render page

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Routes from './components/routes';
-import './App.css';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="content">
-        <Routes />
-      </div>
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
