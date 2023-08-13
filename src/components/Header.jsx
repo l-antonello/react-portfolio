6379e51ae13df45e2b43a7e7cbbea1f8979931a4
otterslacks <antonelloluke@gmail.com>
2023-08-13 15:07:29 -0400

    edits to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,14 @@ const Header = () => {
       <h1>Luke's Portfolio</h1>
       <nav>
         <NavLink exact to="/" activeClassName="active">About Me</NavLink>
-        <NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink>
+        <NavLink
+          to="/portfolio"
+          className={({ isActive, isPending }) =>
+            isPending ? 'pending' : isActive ? 'active' : ''
+          }
+        >
+          Portfolio
+        </NavLink>
         <NavLink to="/contact" activeClassName="active">Contact</NavLink>
         <NavLink to="/resume" activeClassName="active">Resume</NavLink>
       </nav>
